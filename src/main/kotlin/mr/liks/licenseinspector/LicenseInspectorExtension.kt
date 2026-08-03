6dad183bd8f0f1ea8d203cc043c3569bbae10453
Liks <liksluter@yandex.com>
2026-08-03 18:42:43 +0300

    Add CI workflow setup

--- src/main/kotlin/mr/liks/licenseinspector/LicenseInspectorExtension.kt
+++ src/main/kotlin/mr/liks/licenseinspector/LicenseInspectorExtension.kt
@@ -1,8 +1,6 @@
 package mr.liks.licenseinspector
 
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
-import javax.inject.Inject
 
 /** Расширение для настройки плагина [LicenseInspectorPlugin] */
 abstract class LicenseInspectorExtension {
